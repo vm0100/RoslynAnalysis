6a71ce767768d10461e43ae5bc5f90324fc6d053
曾卫 <zengw01@mingyuanyun.com>
2023-03-29 12:48:25 +0800

    函数重写规则待开发

--- RoslynAnalysis.Convert/Rewriter/FunctionBodyRewriter.cs
+++ RoslynAnalysis.Convert/Rewriter/FunctionBodyRewriter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace RoslynAnalysis.Convert.Rewriter;
 
@@ -19,88 +20,50 @@ public class FunctionBodyRewriter : CSharpSyntaxRewriter
         {
             return base.VisitMemberAccessExpression(node);
         }
+        
+        var newNode = RewriterRepositoryMemberAccess(node);
+        if (newNode.IsKinds(SyntaxKind.SimpleMemberAccessExpression, SyntaxKind.PointerMemberAccessExpression))
+        {
+            return newNode;
+        }
 
-        return base.Visit(node.Expression);
+        return base.VisitMemberAccessExpression(newNode as MemberAccessExpressionSyntax);
     }
 
-    //public FunctionBodyWalker(BlockSyntax declaration) : base(declaration)
-    //{
-    //}
+    public SyntaxNode RewriterRepositoryMemberAccess(MemberAccessExpressionSyntax node)
+    {
+        var newNode = node;
 
-    //public static FunctionBodyWalker Build(BlockSyntax blockSyntax) => new FunctionBodyWalker(blockSyntax);
+        // 只处理 entityService.Instance 或者 repository.Value的节点
+        // 重写为 entityService 和 repository
+        var isRepository = newNode.Expression is IdentifierNameSyntax identifier && identifier.Identifier.ValueText.InEndsWithIgnoreCase("EntityService", "Repository");
+        if (isRepository == false || newNode.Name.Identifier.ValueText.In("Instance", "Value") == false)
+        {
+            return newNode;
+        }
+        
+        return base.Visit(newNode.Expression);
+    }
 
-    //public override BlockSyntax Rewriter()
-    //{
+    public override SyntaxNode VisitAssignmentExpression(AssignmentExpressionSyntax node)
+    {
+        return base.VisitAssignmentExpression(node);
+    }
 
-    //    /**
-    //     * 1. 解析属性调用 因为实体服务肯定会访问Instance或者Value
-    //     * 2. 解析属性调用名称是Repository结尾，EntityService结尾的并且有调用Instance和Value的调用
-    //     * 3. API映射，待定。。
-    //     * 4. Lambda表达式转Wrapper
-    //     * 5. 解析后续lambda调用
-    //     *
-    //     * 实体服务调用可能存在位置：
-    //     * 1. 方法调用参数
-    //     * 2. if判断语句
-    //     * 3. block行
-    //     * 4. 简单lambda表达式
-    //     * 5. 对象初始化
-    //     * 6. 字段赋值
-    //     */
+    public override SyntaxNode VisitInvocationExpression(InvocationExpressionSyntax node)
+    {
+        // 只处理 entityService.xx 或者 repository.xx 并且参数是单行lambda的语句
+        var isRepository = node.Expression is MemberAccessExpressionSyntax memberAccess
+                            && memberAccess.Expression is IdentifierNameSyntax identifier
+                            && identifier.Identifier.ValueText.InEndsWithIgnoreCase("EntityService", "Repository");
+        var isArgumentLambda = node.ArgumentList.Arguments.FirstOrDefault()?.Expression.IsKind(SyntaxKind.SimpleLambdaExpression);
+        if (isRepository == false || isArgumentLambda == false)
+        {
+            return base.VisitInvocationExpression(node);
+        }
 
-    //    RewriterRepositoryMemberAccess()
-    //        .RewriterRepositoryLambdaQuery();
-
-    //    return base.Rewriter();
-    //}
-
-    //public FunctionBodyWalker RewriterRepositoryMemberAccess()
-    //{
-    //    foreach (var memberAccess in _declaration.DescendantNodes().OfType<MemberAccessExpressionSyntax>())
-    //    {
-    //        // 只处理 entityService.Instance 或者 repository.Value的节点
-    //        // 重写为 entityService 和 repository
-    //        var isRepository = memberAccess.Expression is IdentifierNameSyntax identifier && identifier.Identifier.ValueText.InEndsWithIgnoreCase("EntityService", "Repository");
-    //        if (isRepository == false || memberAccess.Name.Identifier.ValueText.In("Instance", "Value") == false)
-    //        {
-    //            continue;
-    //        }
-
-    //        _declaration = _declaration.ReplaceNode(memberAccess, memberAccess.Expression);
-    //    }
-
-    //    return this;
-    //}
-
-    //public FunctionBodyWalker RewriterSimpleAssignment()
-    //{
-    //    var nodes = _declaration.DescendantNodes().OfType<AssignmentExpressionSyntax>();
-
-
-
-    //    return this;
-    //}
-
-    //public FunctionBodyWalker RewriterRepositoryLambdaQuery()
-    //{
-    //    var nodes = _declaration.DescendantNodes().OfType<InvocationExpressionSyntax>();
-    //    var invocationExpList = nodes.Where(a => a.IsKind(SyntaxKind.InvocationExpression)).Select(exp => exp as InvocationExpressionSyntax).ToList();
-
-    //    foreach (var invocationExp in _declaration.DescendantNodes().OfType<InvocationExpressionSyntax>())
-    //    {// 只处理 entityService.xx 或者 repository.xx 并且参数是单行lambda的语句
-    //        var isRepository = invocationExp.Expression is MemberAccessExpressionSyntax memberAccess 
-    //                            && memberAccess.Expression is IdentifierNameSyntax identifier 
-    //                            && identifier.Identifier.ValueText.InEndsWithIgnoreCase("EntityService", "Repository");
-    //        var isArgumentLambda = invocationExp.ArgumentList.Arguments.FirstOrDefault()?.Expression.IsKind(SyntaxKind.SimpleLambdaExpression);
-    //        if (isRepository == false || isArgumentLambda == false)
-    //        {
-    //            continue;
-    //        }
-
-    //        // 1. 待判定函数参数格式
-    //        // 2. 待判定函数映射关系
-    //    }
-
-    //    return this;
-    //}
+        // 1. 待判定函数参数格式
+        // 2. 待判定函数映射关系
+        return base.VisitInvocationExpression(node);
+    }
 }
--- RoslynAnalysis.Convert/Rewriter/FunctionRewriter.cs
+++ RoslynAnalysis.Convert/Rewriter/FunctionRewriter.cs
@@ -8,8 +8,21 @@ public class FunctionRewriter : CSharpSyntaxRewriter
 {
     public override SyntaxNode VisitBlock(BlockSyntax node)
     {
-        // DOTO: 移除virtual
-
         return base.Visit(new FunctionBodyRewriter().Visit(node));
+    }
+
+    public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+    {
+        var newNode = node;
+        newNode = newNode.WithModifiers(SyntaxFactory.TokenList(newNode.Modifiers.Where(m => m.IsKind(SyntaxKind.VirtualKeyword))));
+
+        return base.VisitMethodDeclaration(newNode);
+    }
+
+    public override SyntaxNode VisitLocalFunctionStatement(LocalFunctionStatementSyntax node)
+    {
+        var newNode = node;
+        newNode = newNode.WithModifiers(SyntaxFactory.TokenList(newNode.Modifiers.Where(m => m.IsKind(SyntaxKind.VirtualKeyword))));
+        return base.VisitLocalFunctionStatement(newNode);
     }
 }
